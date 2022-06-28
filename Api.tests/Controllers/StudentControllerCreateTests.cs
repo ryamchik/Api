299211ec249cdeca840e1361b467e243c70ec466
Roman <rom.vorobyov@gmail.com>
2022-06-28 16:12:34 +0300

    Add student controller and test

---
+++ Api.tests/Controllers/StudentControllerCreateTests.cs
@@ -0,0 +1,35 @@
+﻿using Api.Controllers;
+using Api.Models;
+using Newtonsoft.Json;
+using System.Web.Http;
+
+namespace Api.tests.Controllers
+{
+    public class StudentControllerCreateTests
+    {
+        [Theory]
+        [InlineData("Иванов Иван Иванович", "22.02.1998", "06:33:22", "J5121")]
+        [InlineData("Иванов Иван Иванович", "12", "06:33:22", "J5121")]
+        public void CreateStudent(string name, string date, string time, string group)
+        {
+            var guis = new Guid();
+            Student trueStudent = new Student
+            {
+                Id = guis,
+                Name = name,
+                Date = date,
+                Time = time,
+                Group = group
+            };
+
+            var controller = new StudentController();
+            var testStudent = controller.Post(name, date, time, group, guis);
+
+            var expectation = JsonConvert.SerializeObject(trueStudent);
+            var reality = JsonConvert.SerializeObject(testStudent);
+
+            Assert.NotNull(testStudent);
+            Assert.Equal(expectation, reality);
+        }
+    }
+}
---
+++ Api/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    [Route("api/student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public Student Post([FromBody] string name, string date, string time, string group, Guid guis = new Guid())
+        {
+            Student newStudent = new Student {
+                Id = new Guid(),
+                Name = name,
+                Date = date,
+                Time = time,
+                Group = group
+            };
+
+            return newStudent;
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
