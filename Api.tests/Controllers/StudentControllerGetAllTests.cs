3c433da4fb7dc6ad034016c03654e8c94b10330f
Roman <rom.vorobyov@gmail.com>
2022-06-28 17:51:41 +0300

    Add test for get all students method

---
+++ Api.tests/Controllers/StudentControllerGetAllTests.cs
@@ -0,0 +1,15 @@
+﻿using Api.Controllers;
+
+namespace Api.tests.Controllers
+{
+    public class StudentControllerGetAllTests
+    {
+        [Fact]
+        public void GetAll()
+        {
+            var controller = new StudentController();
+            var students = controller.Get();
+            Assert.NotNull(students);
+        }
+    }
+}
