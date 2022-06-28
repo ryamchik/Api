05590d99e4a5c034d48ac26ff09f58dd9564eebe
Roman <rom.vorobyov@gmail.com>
2022-06-28 13:10:33 +0300

    Add model classes

---
+++ Api/Models/Group.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Models
+{
+    public class Group
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Api/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }  
+        public DateOnly Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Group { get; set; }
+    }
+}
